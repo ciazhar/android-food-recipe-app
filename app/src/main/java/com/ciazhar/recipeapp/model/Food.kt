0aa024cd34de44ac4f89f048bece187115d7d0f5
ciazhar <daisetsunakama@gmail.com>
2017-10-03 20:43:15 +0700

    create model food

---
+++ app/src/main/java/com/ciazhar/recipeapp/model/Food.kt
@@ -0,0 +1,13 @@
+package com.ciazhar.recipeapp.model
+
+/**
+* Created by ciazhar on 10/2/17.
+*
+* [ Documentation Here]
+*/
+
+data class Food(
+        var foodName : String,
+        var foodImage : Int,
+        var foodDescription : String
+)
